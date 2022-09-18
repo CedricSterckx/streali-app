b0837239b9ca7fbe5b8460710afe72220a9333f4
Wiliam Traoré <hi@wil-liam.com>
2022-09-19 00:14:32 +0200

    chore(project): add eslint + prettier + husky

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<div>ok</div>
-	</React.StrictMode>
+  <React.StrictMode>
+    <div>ok</div>
+  </React.StrictMode>
 );
