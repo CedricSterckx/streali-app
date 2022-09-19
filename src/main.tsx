047d944c9e41203288474ceb2766eeab3803b088
Thibault <contactazarox@gmail.com>
2022-09-19 12:11:30 +0200

    feat(project): implement reactQuery

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './main.scss';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>ok</div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} position={'bottom-right'} />
+      <div>ok</div>
+    </QueryClientProvider>
   </React.StrictMode>
 );
